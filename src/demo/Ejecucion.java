c075f57fb93023ddc68ef55eed6fb6379d72b74a
i201816457 <i201816457@cibertec.edu.pe>
2020-11-10 18:27:26 -0500

    mi primer commit

---
+++ src/demo/Ejecucion.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Ejecucion {
+
+	public static void main(String[] args) {
+		int x=1;
+		int y=2;
+		int z=3;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+	}
+}
